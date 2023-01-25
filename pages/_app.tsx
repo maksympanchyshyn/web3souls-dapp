2bcccee93a0f186b0bdf31e334d4090151a8a866
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2023-01-25 12:52:27 +0200

    Add next-ui with next-themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { ThemeProvider } from 'next-themes';
+import type { AppProps } from 'next/app';
+
+const lightTheme = createTheme({ type: 'light' });
+const darkTheme = createTheme({ type: 'dark' });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </ThemeProvider>
+  );
 }
